88c2b5f2f633855e04cdcac75b7ba3ff4a94c3e4
Amardeepsingh Siglani <amardeep7194@gmail.com>
2022-10-15 12:40:01 -0700

    implemented plugin server component with create detector support
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- models/interfaces.ts
+++ models/interfaces.ts
@@ -7,15 +7,11 @@ export interface Detector {
   type: string;
   detector_type: string;
   name: string;
-  alert_conditions: AlertCondition[];
   enabled: boolean;
+  createdBy: string;
   schedule: PeriodSchedule;
   inputs: DetectorInput[];
-  enabled_time?: number;
-  createdBy?: string;
-  last_update_time?: number;
-  monitor_id?: string;
-  rule_topic_index?: string;
+  triggers: AlertCondition[];
 }
 
 export interface PeriodSchedule {
--- server/models/interfaces.ts
+++ server/models/interfaces.ts
@@ -1,8 +1,31 @@
-export interface DefaultHeaders {
-  "Content-Type": "application/json";
-  Accept: "application/json";
-}
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Detector } from '../../models/interfaces';
+import DetectorsService from '../services/DetectorService';
 
 export interface SecurityAnalyticsApi {
-  [API_ROUTE: string]: string;
+  readonly DETECTORS_BASE: string;
+}
+
+export interface NodeServices {
+  detectorsService: DetectorsService;
+}
+
+export interface CreateDetectorParams {
+  body: Detector;
+}
+
+export interface CreateDetectorResponse {
+  _id: string;
+  _version: number;
+  detector: {
+    detector: Detector & {
+      last_update_time: number;
+      monitor_id: string;
+      rule_topic_index: string;
+    };
+  };
 }
