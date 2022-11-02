1695cb21fe59d02606ea9abdc57cc89e8500fdad
bowenlan-amzn <bowenlan23@gmail.com>
2022-11-02 15:33:18 -0700

    Add unit test CI (#34)
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>
    
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>
    (cherry picked from commit d05c2380fe7ee709f8396d86290ee0eeb0c7beb7)

---
+++ test/mocks/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import browserServicesMock from './browserServicesMock';
+import httpClientMock from './httpClientMock';
+
+export { browserServicesMock, httpClientMock };
