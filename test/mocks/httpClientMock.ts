150f7ad22c21f73a79d738aa2b6a59b04a01eed4
bowenlan-amzn <bowenlan23@gmail.com>
2022-11-02 15:33:18 -0700

    Add unit test CI (#34)
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>
    
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

---
+++ test/mocks/httpClientMock.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { HttpSetup } from 'opensearch-dashboards/public';
+
+const httpClientMock = jest.fn() as any;
+
+httpClientMock.delete = jest.fn();
+httpClientMock.get = jest.fn();
+httpClientMock.head = jest.fn();
+httpClientMock.post = jest.fn();
+httpClientMock.put = jest.fn();
+
+export default httpClientMock as HttpSetup;
