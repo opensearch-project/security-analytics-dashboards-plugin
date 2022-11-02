d05c2380fe7ee709f8396d86290ee0eeb0c7beb7
bowenlan-amzn <bowenlan23@gmail.com>
2022-11-02 15:33:18 -0700

    Add unit test CI (#34)
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>
    
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>

---
+++ test/setup.jest.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import { configure } from '@testing-library/react';
+
+configure({ testIdAttribute: 'data-test-subj' });
+
+jest.mock('@elastic/eui/lib/eui_components/form/form_row/make_id', () => () => 'some_make_id');
+
+jest.mock('@elastic/eui/lib/services/accessibility/html_id_generator', () => ({
+  htmlIdGenerator: () => {
+    return () => 'some_html_id';
+  },
+}));
+
+// @ts-ignore
+window.Worker = function () {
+  this.postMessage = () => {};
+  // @ts-ignore
+  this.terminate = () => {};
+};
+
+// @ts-ignore
+window.URL = {
+  createObjectURL: () => {
+    return '';
+  },
+};
+
+// https://github.com/elastic/eui/issues/2530
+jest.mock('@elastic/eui/lib/eui_components/icon', () => ({
+  EuiIcon: () => 'EuiIconMock',
+  __esModule: true,
+  IconPropType: require('@elastic/eui/lib/eui_components/icon/icon').IconPropType,
+  ICON_TYPES: require('@elastic/eui/lib/eui_components/icon/icon').TYPES,
+  ICON_SIZES: require('@elastic/eui/lib/eui_components/icon/icon').SIZES,
+  ICON_COLORS: require('@elastic/eui/lib/eui_components/icon/icon').COLORS,
+}));
+
+jest.setTimeout(10000); // in milliseconds
