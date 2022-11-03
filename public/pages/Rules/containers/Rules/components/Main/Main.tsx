9f76fc2199e682c45632b0c26b69ad9d6ad1298e
Amardeepsingh <114732919+amsiglan@users.noreply.github.com>
2022-11-03 12:00:00 -0700

    updated main and rules pages (#41)
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

---
+++ public/pages/Rules/containers/Rules/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import Table from '../Table';
+
+export const Main = () => {
+  return <Table />;
+};
