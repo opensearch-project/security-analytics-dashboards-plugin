f954b89292bc7bf02244882b3dbee942b68c6536
Amardeepsingh <114732919+amsiglan@users.noreply.github.com>
2022-11-03 11:57:48 -0700

    Overview UI page updates (#37)
    * merged latest Overview page
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * minor refactor
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- public/pages/Overview/containers/Overview/Overview.tsx
+++ public/pages/Overview/containers/Overview/Overview.tsx
@@ -3,178 +3,136 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import './Overview.scss';
 import {
-  EuiFieldSearch,
+  EuiButtonEmpty,
   EuiFlexGrid,
   EuiFlexGroup,
   EuiFlexItem,
-  EuiLink,
-  EuiSelect,
-  EuiSelectOption,
-  EuiSpacer,
+  EuiPopover,
   EuiSuperDatePicker,
-  EuiText,
+  EuiTitle,
 } from '@elastic/eui';
-import React, { Component } from 'react';
-import { ContentPanel } from '../../../../components/ContentPanel';
-import { View, parse } from 'vega/build-es5/vega.js';
-import { compile } from 'vega-lite';
-import { BREADCRUMBS, ROUTES } from '../../../../utils/constants';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { BREADCRUMBS } from '../../../../utils/constants';
 import { OverviewProps, OverviewState } from '../../models/interfaces';
-import {
-  dummyAlertItems,
-  dummyDetectorItems,
-  dummyFindingItems,
-  getVisualizationSpec,
-} from '../../utils/dummyData';
-import { groupByOptions } from '../../utils/constants';
 import { CoreServicesContext } from '../../../../../public/components/core_services';
 import { RecentAlertsWidget } from '../../components/Widgets/RecentAlertsWidget';
 import { RecentFindingsWidget } from '../../components/Widgets/RecentFindingsWidget';
-import { WidgetContainer } from '../../components/Widgets/WidgetContainer';
 import { DetectorsWidget } from '../../components/Widgets/DetectorsWidget';
-import { expressionInterpreter as vegaExpressionInterpreter } from 'vega-interpreter/build/vega-interpreter.module';
+import { OverviewViewModel, OverviewViewModelActor } from '../../models/OverviewViewModel';
+import { ServicesContext } from '../../../../services';
+import { Summary } from '../../components/Widgets/Summary';
+import { TopRulesWidget } from '../../components/Widgets/TopRulesWidget';
+import { GettingStartedPopup } from '../../components/GettingStarted/GettingStartedPopup';
 
-export default class Overview extends Component<OverviewProps, OverviewState> {
-  static contextType = CoreServicesContext;
+export const Overview: React.FC<OverviewProps> = (props) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [initialLoadingFinished, setInitialLoadingFinished] = useState(false);
+  const [state, setState] = useState<OverviewState>({
+    groupBy: 'all_findings',
+    overviewViewModel: {
+      detectors: [],
+      findings: [],
+      alerts: [],
+    },
+  });
+  const context = useContext(CoreServicesContext);
+  const services = useContext(ServicesContext);
 
-  constructor(props: OverviewProps) {
-    super(props);
-    this.state = {
-      groupBy: 'all_findings',
+  const updateState = (overviewViewModel: OverviewViewModel) => {
+    setState({
+      ...state,
+      overviewViewModel: { ...overviewViewModel },
+    });
+  };
+
+  const overviewViewModelActor = useMemo(() => new OverviewViewModelActor(services), [services]);
+
+  useEffect(() => {
+    context?.chrome.setBreadcrumbs([BREADCRUMBS.SECURITY_ANALYTICS, BREADCRUMBS.OVERVIEW]);
+    overviewViewModelActor.registerRefreshHandler(updateState);
+
+    const updateModel = async () => {
+      await overviewViewModelActor.onRefresh();
+      setInitialLoadingFinished(true);
     };
-  }
 
-  createSelectComponent(
-    options: EuiSelectOption[],
-    value: string,
-    onChange: React.ChangeEventHandler<HTMLSelectElement>
-  ) {
-    return (
-      <EuiFlexGroup justifyContent="flexEnd" alignItems="center">
-        <EuiFlexItem grow={false}>
-          <h5>Group by</h5>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiSelect
-            id="overview-vis-options"
-            options={options}
-            value={value}
-            onChange={onChange}
-          />
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    );
-  }
+    updateModel();
+  }, []);
 
-  generateVisualizationSpec() {
-    return getVisualizationSpec();
-  }
-
-  renderVis() {
-    let view;
-    const spec = this.generateVisualizationSpec();
-
-    try {
-      renderVegaSpec(
-        compile({ ...spec, width: 'container', height: 400 }).spec
-      ).catch((err: Error) => console.error(err));
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    if (
+      !props.getStartedDismissedOnce &&
+      initialLoadingFinished &&
+      state.overviewViewModel.detectors.length === 0
+    ) {
+      setIsPopoverOpen(true);
     }
+  }, [initialLoadingFinished, state.overviewViewModel, props.getStartedDismissedOnce]);
 
-    function renderVegaSpec(spec: {}) {
-      view = new View(parse(spec, null, { expr: vegaExpressionInterpreter }), {
-        renderer: 'canvas', // renderer (canvas or svg)
-        container: '#view', // parent DOM container
-        hover: true, // enable hover processing
-      });
-      return view.runAsync();
-    }
-  }
+  const onTimeChange = ({ start, end }: { start: string; end: string }) => {
+    // TODO: NYI
+  };
 
-  componentDidMount(): void {
-    this.context.chrome.setBreadcrumbs([BREADCRUMBS.SECURITY_ANALYTICS, BREADCRUMBS.OVERVIEW]);
-    this.renderVis();
-  }
+  const onRefresh = async () => {
+    overviewViewModelActor.onRefresh();
+  };
 
-  createVisualizationActions(): React.ReactNode[] {
-    return [
-      this.createSelectComponent(groupByOptions, this.state.groupBy, (event) => {
-        this.setState({ groupBy: event.target.value });
-      }),
-    ];
-  }
+  const onButtonClick = () => setIsPopoverOpen((isPopoverOpen) => !isPopoverOpen);
+  const closePopover = () => {
+    setIsPopoverOpen(false);
+    props.onGetStartedDismissed();
+  };
 
-  onTimeChange = ({ start, end }: { start: string; end: string }) => {};
+  const button = (
+    <EuiButtonEmpty iconType="cheer" onClick={onButtonClick}>
+      Getting started
+    </EuiButtonEmpty>
+  );
 
-  onRefresh = async () => {};
-
-  render() {
-    return (
-      <ContentPanel title={'Overview'}>
-        <EuiFlexGroup direction="column">
+  return (
+    <EuiFlexGroup direction="column">
+      <EuiFlexItem style={{ alignSelf: 'flex-end' }}>
+        <EuiPopover
+          button={button}
+          isOpen={isPopoverOpen}
+          anchorPosition="downRight"
+          closePopover={closePopover}
+        >
+          <GettingStartedPopup dismissPopup={closePopover} history={props.history} />
+        </EuiPopover>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiFlexGroup justifyContent="spaceBetween">
           <EuiFlexItem>
-            <EuiFlexGroup gutterSize={'s'}>
-              <EuiFlexItem grow={9}>
-                <EuiFieldSearch
-                  fullWidth={true}
-                  // onChange={this.onSearchChange}
-                  placeholder={'Search findings'}
-                />
-              </EuiFlexItem>
-              <EuiFlexItem grow={1}>
-                <EuiSuperDatePicker onTimeChange={this.onTimeChange} onRefresh={this.onRefresh} />
-              </EuiFlexItem>
-            </EuiFlexGroup>
-            <EuiSpacer size={'m'} />
+            <EuiTitle size="l">
+              <h1>Overview</h1>
+            </EuiTitle>
           </EuiFlexItem>
-          <EuiFlexItem style={{ padding: 20 }}>
-            <WidgetContainer
-              title="Findings and alert count"
-              actions={this.createVisualizationActions()}
-            >
-              <EuiFlexGroup gutterSize="s" direction="column">
-                <EuiFlexItem>
-                  <EuiFlexGroup gutterSize="xl">
-                    <EuiFlexItem grow={false}>
-                      <EuiText size="s">
-                        <p>Total active alerts</p>
-                      </EuiText>
-                      <EuiLink href={`#${ROUTES.RULES}`} className="page-link">
-                        43
-                      </EuiLink>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={false}>
-                      <EuiText size="s">
-                        <p>Total findings</p>
-                      </EuiText>
-                      <EuiLink href={`#${ROUTES.FINDINGS}`} className="page-link">
-                        323
-                      </EuiLink>
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiFlexItem>
-                <EuiFlexItem>
-                  <div id="view" style={{ width: '100%' }}></div>
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            </WidgetContainer>
-          </EuiFlexItem>
-
           <EuiFlexItem>
-            <EuiFlexGrid columns={2} gutterSize="m">
-              <RecentAlertsWidget items={dummyAlertItems} />
-              <RecentFindingsWidget items={dummyFindingItems} />
-              <WidgetContainer title="Top rules count from findings">
-                <div id="#top_rules_vis" />
-              </WidgetContainer>
-              <DetectorsWidget items={dummyDetectorItems} />
-            </EuiFlexGrid>
+            <EuiSuperDatePicker
+              onTimeChange={onTimeChange}
+              onRefresh={onRefresh}
+              updateButtonProps={{ fill: false }}
+            />
           </EuiFlexItem>
         </EuiFlexGroup>
-      </ContentPanel>
-    );
-  }
-}
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <Summary
+          alerts={state.overviewViewModel.alerts}
+          findings={state.overviewViewModel.findings}
+        />
+      </EuiFlexItem>
+
+      <EuiFlexItem>
+        <EuiFlexGrid columns={2} gutterSize="m">
+          <RecentAlertsWidget items={state.overviewViewModel.alerts} />
+          <RecentFindingsWidget items={state.overviewViewModel.findings} />
+          <TopRulesWidget findings={state.overviewViewModel.findings} />
+          <DetectorsWidget detectorHits={state.overviewViewModel.detectors} {...props} />
+        </EuiFlexGrid>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
