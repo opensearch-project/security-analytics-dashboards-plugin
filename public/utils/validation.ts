d374eff2be3c7098c9c99de08460d1852cf94d1b
AWSHurneyt <hurneyt@amazon.com>
2022-11-07 18:39:11 -0500

    Implemented input validation for the detector description field. (#84)
    * Implemented input validation for the detector description field.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed typo.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed error message call.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    (cherry picked from commit 9b16ac512e3403645b23017034b51f37bd4ccd4e)

--- public/utils/validation.ts
+++ public/utils/validation.ts
@@ -6,8 +6,8 @@
 export const MIN_NAME_CHARACTERS = 5;
 export const MAX_NAME_CHARACTERS = 50;
 
-// This regex supports MIN to MAX character limit, capital and lowercase letters,
-// numbers 0-9, spaces, hyphens and underscores.
+// This regex pattern support MIN to MAX character limit, capital and lowercase letters,
+// numbers 0-9, hyphens, spaces, and underscores.
 export const NAME_REGEX = new RegExp(/^[a-zA-Z0-9 _-]{5,50}$/);
 
 /**
@@ -29,7 +29,34 @@ export function getNameErrorMessage(
     if (trimmedName.length === 0) return 'Enter a name.';
     if (nameIsInvalid)
       return `Names consist of ${MIN_NAME_CHARACTERS}-${MAX_NAME_CHARACTERS} characters,
-      upper and lowercase letters, numbers 0-9, hyphens, underscores, and spaces.`;
+      upper and lowercase letters, numbers 0-9, hyphens, spaces, and underscores.`;
+  }
+}
+
+export const MIN_DESCRIPTION_CHARACTERS = 0;
+export const MAX_DESCRIPTION_CHARACTERS = 500;
+
+// This regex pattern support MIN to MAX character limit, capital and lowercase letters,
+// numbers 0-9, hyphens, periods, spaces, and underscores.
+export const DESCRIPTION_REGEX = new RegExp(/^[a-zA-Z0-9 _.,-]{0,500}$/);
+
+/**
+ * Validates a string against NAME_REGEX.
+ * @param name: The string to validate.
+ * @return TRUE if valid; else FALSE.
+ */
+export function validateDescription(name: string): boolean {
+  return name.trim().match(DESCRIPTION_REGEX) !== null;
+}
+
+export function getDescriptionErrorMessage(
+  description: string,
+  descriptionIsInvalid: boolean,
+  descriptionFieldTouched: boolean
+) {
+  if (descriptionFieldTouched && descriptionIsInvalid) {
+    return `Descriptions consist of ${MIN_DESCRIPTION_CHARACTERS}-${MAX_DESCRIPTION_CHARACTERS} characters,
+      upper and lowercase letters, numbers 0-9, commas, hyphens, periods, spaces, and underscores.`;
   }
 }
 
