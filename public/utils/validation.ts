7b1c334538e50110c89b7bdd2a4987a5e9630124
Amardeepsingh Siglani <amardeep7194@gmail.com>
2022-11-08 16:22:29 -0800

    bug fixes
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/utils/validation.ts
+++ public/utils/validation.ts
@@ -10,14 +10,23 @@ export const MAX_NAME_CHARACTERS = 50;
 // numbers 0-9, hyphens, spaces, and underscores.
 export const NAME_REGEX = new RegExp(/^[a-zA-Z0-9 _-]{5,50}$/);
 
+// This regex pattern support MIN to MAX character limit, capital and lowercase letters,
+// numbers 0-9, hyphens, spaces, and underscores.
+export const AUTHOR_REGEX = new RegExp(/^[a-zA-Z0-9 _,-]{5,50}$/);
+
 /**
  * Validates a string against NAME_REGEX.
  * @param name: The string to validate.
  * @return TRUE if valid; else FALSE.
  */
-export function validateName(name: string): boolean {
-  return name.trim().match(NAME_REGEX) !== null;
+export function validateName(name: string, regex: RegExp = NAME_REGEX): boolean {
+  return name.trim().match(regex) !== null;
 }
+
+export const nameErrorString = `Name should only consist of upper and lowercase letters, numbers 0-9, hyphens, spaces, and underscores. 
+Use between ${MIN_NAME_CHARACTERS} and ${MAX_NAME_CHARACTERS} characters.`;
+export const authorErrorString = `Author name should only consist of upper and lowercase letters, numbers 0-9, hyphens, commas, spaces, and underscores. 
+Use between ${MIN_NAME_CHARACTERS} and ${MAX_NAME_CHARACTERS} characters.`;
 
 export function getNameErrorMessage(
   name: string,
@@ -27,9 +36,7 @@ export function getNameErrorMessage(
   const trimmedName = name.trim();
   if (nameFieldTouched) {
     if (trimmedName.length === 0) return 'Enter a name.';
-    if (nameIsInvalid)
-      return `Names consist of ${MIN_NAME_CHARACTERS}-${MAX_NAME_CHARACTERS} characters,
-      upper and lowercase letters, numbers 0-9, hyphens, spaces, and underscores.`;
+    if (nameIsInvalid) return nameErrorString;
   }
 }
 
@@ -49,14 +56,16 @@ export function validateDescription(name: string): boolean {
   return name.trim().match(DESCRIPTION_REGEX) !== null;
 }
 
+export const descriptionErrorString = `Description should only consist of upper and lowercase letters, numbers 0-9,
+ commas, hyphens, periods, spaces, and underscores. Max limit of ${MAX_DESCRIPTION_CHARACTERS} characters,`;
+
 export function getDescriptionErrorMessage(
-  description: string,
+  _description: string,
   descriptionIsInvalid: boolean,
   descriptionFieldTouched: boolean
 ) {
   if (descriptionFieldTouched && descriptionIsInvalid) {
-    return `Descriptions consist of ${MIN_DESCRIPTION_CHARACTERS}-${MAX_DESCRIPTION_CHARACTERS} characters,
-      upper and lowercase letters, numbers 0-9, commas, hyphens, periods, spaces, and underscores.`;
+    return descriptionErrorString;
   }
 }
 
