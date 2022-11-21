c53720c9bb25f3adb2d44604b8e57525171b8c02
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2022-11-21 13:08:57 +0100

    [FEATURE] Get started | Add Alert info in the Get started pop-up #129
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- public/pages/Overview/components/GettingStarted/GettingStartedPopup.tsx
+++ public/pages/Overview/components/GettingStarted/GettingStartedPopup.tsx
@@ -4,25 +4,16 @@
  */
 
 import React, { useMemo } from 'react';
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiHorizontalRule,
-  EuiSpacer,
-  EuiSteps,
-  EuiText,
-  EuiTitle,
-} from '@elastic/eui';
-import { EuiButton } from '@elastic/eui';
+import { EuiHorizontalRule, EuiSpacer, EuiSteps, EuiText, EuiTitle } from '@elastic/eui';
 import { EuiContainedStepProps } from '@elastic/eui/src/components/steps/steps';
 import { ROUTES } from '../../../../utils/constants';
 import { RouteComponentProps } from 'react-router-dom';
+import { GetStartedStep } from './GetStartedStep';
 
 export interface GettingStartedPopupProps {
   dismissPopup: () => void;
   history: RouteComponentProps['history'];
 }
-export interface OverviewStartState {}
 
 export const GettingStartedPopup: React.FC<GettingStartedPopupProps> = ({
   dismissPopup,
@@ -33,88 +24,86 @@ export const GettingStartedPopup: React.FC<GettingStartedPopupProps> = ({
       {
         title: 'Create security detector',
         children: (
-          <div style={{ marginTop: '-20px' }}>
-            <EuiText>
-              <p>
-                Identify security findings and threats from your log datas with curated detection
-                rules.
-              </p>
-            </EuiText>
-            <EuiSpacer size="s" />
-            <EuiButton
-              fill
-              onClick={() => {
-                dismissPopup();
-                history.push(ROUTES.DETECTORS_CREATE);
-              }}
-            >
-              Create detector
-            </EuiButton>
-          </div>
+          <GetStartedStep
+            title={
+              'Identify security findings and threats from your log datas with curated detection rules.'
+            }
+            buttons={[
+              {
+                text: 'Create detector',
+                onClick: () => {
+                  dismissPopup();
+                  history.push(ROUTES.DETECTORS_CREATE);
+                },
+                opts: {
+                  fill: true,
+                },
+              },
+            ]}
+          />
         ),
       },
       {
         title: 'Discover security findings',
         children: (
-          <div style={{ marginTop: '-20px' }}>
-            <EuiText>
-              <p>
-                After detectors are create, you can view insights and analyse security findings.
-              </p>
-            </EuiText>
-            <EuiSpacer size="s" />
-            <EuiFlexGroup gutterSize="s">
-              <EuiFlexItem grow={false}>
-                <EuiButton fill onClick={dismissPopup}>
-                  Overview
-                </EuiButton>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiButton
-                  onClick={() => {
-                    dismissPopup();
-                    history.push(ROUTES.FINDINGS);
-                  }}
-                >
-                  View findings
-                </EuiButton>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </div>
+          <GetStartedStep
+            title={
+              'After detectors are create, you can view insights and analyse security findings.'
+            }
+            buttons={[
+              {
+                text: 'Overview',
+                onClick: () => dismissPopup(),
+                opts: {
+                  fill: true,
+                },
+              },
+              {
+                text: 'View findings',
+                onClick: () => {
+                  dismissPopup();
+                  history.push(ROUTES.FINDINGS);
+                },
+              },
+            ]}
+          />
+        ),
+      },
+      {
+        title: 'View security alerts',
+        children: (
+          <GetStartedStep
+            title={'After detectors are created, you can view alerts triggered by findings.'}
+            buttons={[
+              {
+                text: 'View alerts',
+                onClick: () => {
+                  dismissPopup();
+                  history.push(ROUTES.ALERTS);
+                },
+                opts: {
+                  fill: true,
+                },
+              },
+            ]}
+          />
         ),
       },
       {
         title: 'Create custom rules for detectors',
         children: (
-          <div style={{ marginTop: '-20px' }}>
-            <EuiText>
-              <p>Create rule or fine tune existing rules that can be added to detectors.</p>
-            </EuiText>
-            <EuiSpacer size="s" />
-            <EuiFlexGroup gutterSize="s">
-              <EuiFlexItem grow={false}>
-                <EuiButton
-                  fill
-                  onClick={() => {
-                    dismissPopup();
-                    history.push(ROUTES.RULES_CREATE);
-                  }}
-                >
-                  Create rule
-                </EuiButton>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiButton
-                  onClick={() => {
-                    dismissPopup();
-                    history.push(ROUTES.RULES);
-                  }}
-                >
-                  Manage rules
-                </EuiButton>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </div>
+          <GetStartedStep
+            title={'Create rule or fine tune existing rules that can be added to detectors.'}
+            buttons={[
+              {
+                text: 'Create rule',
+                onClick: () => {
+                  dismissPopup();
+                  history.push(ROUTES.RULES_CREATE);
+                },
+              },
+            ]}
+          />
         ),
       },
     ],
