d81106845bb18a9932a2c105af3c84c15f6c4cf1
paladin-tech <johnsie@gmail.com>
2022-11-23 22:22:38 +0100

    Fix button color (#169)
    Signed-off-by: Sinisa Andric <johnsie@gmail.com>
    
    Signed-off-by: Sinisa Andric <johnsie@gmail.com>
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- public/pages/CreateDetector/components/ConfigureAlerts/containers/ConfigureAlerts.tsx
+++ public/pages/CreateDetector/components/ConfigureAlerts/containers/ConfigureAlerts.tsx
@@ -121,7 +121,9 @@ export default class ConfigureAlerts extends Component<ConfigureAlertsProps, Con
                 paddingSize={'none'}
                 initialIsOpen={true}
                 extraAction={
-                  <EuiButton onClick={() => this.onDelete(index)}>Remove alert trigger</EuiButton>
+                  <EuiButton color="danger" onClick={() => this.onDelete(index)}>
+                    Remove alert trigger
+                  </EuiButton>
                 }
               >
                 <EuiHorizontalRule margin={'xs'} />
