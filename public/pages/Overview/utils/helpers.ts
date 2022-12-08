a915f855a624a12d27f4ba3bae5d4b2f4fdd7288
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2022-12-08 09:49:35 +0100

    [FEATURE] Charts | Change charts time-unit to bigger timespan #164 (#204)
    * [FEATURE] Charts | Change charts time-unit to bigger timespan #164
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Charts | Change charts time-unit to bigger timespan #164
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- public/pages/Overview/utils/helpers.ts
+++ public/pages/Overview/utils/helpers.ts
@@ -3,9 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import moment from 'moment';
 import { euiPaletteColorBlind, euiPaletteForStatus } from '@elastic/eui';
 import { TopLevelSpec } from 'vega-lite';
 import { SummaryData } from '../components/Widgets/Summary';
+import dateMath from '@elastic/datemath';
+import { TimeUnitsMap } from './constants';
+import _ from 'lodash';
+
+export type DateOpts = {
+  timeUnit: string;
+  dateFormat: string;
+};
 
 function getVisualizationSpec(description: string, data: any, layers: any[]): TopLevelSpec {
   return {
@@ -21,11 +30,25 @@ function getVisualizationSpec(description: string, data: any, layers: any[]): To
 
 export function getOverviewVisualizationSpec(
   visualizationData: SummaryData[],
-  groupBy: string
+  groupBy: string,
+  dynamicTimeUnit: string = 'yearmonthdatehoursminutes'
 ): TopLevelSpec {
-  const timeUnit = 'yearmonthdatehoursminutes';
+  const timeUnit = dynamicTimeUnit;
   const aggregate = 'sum';
-  const groupByLogType = groupBy === 'logType';
+  const findingsEncoding: { [x: string]: any } = {
+    x: { timeUnit, field: 'time', title: '', axis: { grid: false, ticks: false } },
+    y: {
+      aggregate,
+      field: 'finding',
+      type: 'quantitative',
+      title: 'Count',
+      axis: { grid: true, ticks: false },
+    },
+  };
+
+  if (groupBy === 'log_type') {
+    findingsEncoding['color'] = { field: 'logType', type: 'nominal', title: 'Log type' };
+  }
 
   return getVisualizationSpec(
     'Plot showing average data with raw values in the background.',
@@ -33,24 +56,7 @@ export function getOverviewVisualizationSpec(
     [
       {
         mark: 'bar',
-        encoding: {
-          x: { timeUnit, field: 'time', title: '', axis: { grid: false, ticks: false } },
-          y: {
-            aggregate,
-            field: 'finding',
-            type: 'quantitative',
-            title: 'Count',
-            axis: { grid: true, ticks: false },
-          },
-          color: {
-            field: groupByLogType ? 'logType' : 'finding',
-            type: 'nominal',
-            title: groupByLogType ? 'Log type' : 'All findings',
-            scale: {
-              range: euiPaletteColorBlind(),
-            },
-          },
-        },
+        encoding: findingsEncoding,
       },
       {
         mark: {
@@ -66,16 +72,63 @@ export function getOverviewVisualizationSpec(
   );
 }
 
-export function getFindingsVisualizationSpec(visualizationData: any[], groupBy: string) {
+/**
+ * Returns chart x-axis date format based on time span
+ * @param start
+ * @param end
+ */
+export function getDateFormatByTimeUnit(start: string, end: string) {
+  const startMoment = dateMath.parse(start);
+  const endMoment = dateMath.parse(end);
+  let dateFormat = '%Y-%m-%d %H:%M';
+
+  if (startMoment && endMoment) {
+    const startData = startMoment.toObject();
+    const endData = endMoment.toObject();
+    const dateDiff = endMoment.diff(startMoment);
+    const momentDiff = moment.duration(dateDiff);
+    const daysDiff = _.get(momentDiff, '_data.days', 0);
+
+    if (startData.years === endData.years) {
+      if (startData.months !== endData.months) {
+        dateFormat = '%Y-%m-%d';
+
+        if (daysDiff < 30 && daysDiff > 1) {
+          dateFormat = '%Y-%m-%d %H:%M';
+        }
+      } else {
+        dateFormat = '%Y-%m-%d %H:%M';
+
+        if (startData.date === endData.date) {
+          dateFormat = '%H:%M:%S';
+        }
+      }
+    }
+  }
+
+  return dateFormat;
+}
+
+export function getFindingsVisualizationSpec(
+  visualizationData: any[],
+  groupBy: string,
+  dateOpts: DateOpts = {
+    timeUnit: 'yearmonthdatehoursminutes',
+    dateFormat: '%Y-%m-%d %H:%M',
+  }
+) {
   return getVisualizationSpec('Findings data overview', visualizationData, [
     {
       mark: 'bar',
       encoding: {
         x: {
-          timeUnit: 'yearmonthdatehoursminutes',
+          timeUnit: dateOpts.timeUnit,
           field: 'time',
           title: '',
-          axis: { grid: false, ticks: false },
+          axis: {
+            grid: false,
+            format: dateOpts.dateFormat,
+          },
         },
         y: {
           aggregate: 'sum',
@@ -97,16 +150,27 @@ export function getFindingsVisualizationSpec(visualizationData: any[], groupBy: 
   ]);
 }
 
-export function getAlertsVisualizationSpec(visualizationData: any[], groupBy: string) {
+export function getAlertsVisualizationSpec(
+  visualizationData: any[],
+  groupBy: string,
+  dateOpts: DateOpts = {
+    timeUnit: 'yearmonthdatehoursminutes',
+    dateFormat: '%Y-%m-%d %H:%M',
+  }
+) {
   return getVisualizationSpec('Alerts data overview', visualizationData, [
     {
       mark: 'bar',
       encoding: {
         x: {
-          timeUnit: 'yearmonthdatehoursminutes',
+          timeUnit: dateOpts.timeUnit,
           field: 'time',
           title: '',
-          axis: { grid: false, ticks: false },
+          axis: {
+            grid: false,
+            ticks: false,
+            format: dateOpts.dateFormat,
+          },
         },
         y: {
           aggregate: 'sum',
@@ -153,4 +217,41 @@ export function getTimeWithMinPrecision(time: number | string) {
   date.setMilliseconds(0);
 
   return date.getTime();
+}
+
+/**
+ * Returns timeUnit based on how big time diff is between start and end dates
+ * @param start Chart start time
+ * @param end Chart end time
+ * @param [defaultUnit = 'yearmonthdatehoursminutes'] Default timeUnit
+ */
+export function getChartTimeUnit(
+  start: string,
+  end: string,
+  defaultUnit: string = 'yearmonthdatehoursminutes'
+): string {
+  const startMoment = dateMath.parse(start);
+  const endMoment = dateMath.parse(end);
+  let minUnit: string = 'minutes';
+  let timeUnit: string = TimeUnitsMap[minUnit];
+
+  if (!startMoment || !endMoment) return defaultUnit;
+
+  try {
+    const timeDiff = endMoment.diff(startMoment);
+    const momentTimeDiff = moment.duration(timeDiff);
+
+    const timeUnits: string[] = ['years', 'months', 'days', 'hours', 'minutes'];
+    for (const unit of timeUnits) {
+      // @ts-ignore
+      if (momentTimeDiff._data[unit]) {
+        timeUnit = TimeUnitsMap[unit];
+        break;
+      }
+    }
+  } catch (e) {
+    console.error(`Time diff can't be calculated for dates: ${start} and ${end}`);
+  }
+
+  return timeUnit;
 }
