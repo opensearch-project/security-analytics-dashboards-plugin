34d9f749f45303ddf7e1610ac49f0962e4d30744
Aleksandar Djindjic <djindjic@gmail.com>
2022-12-16 19:20:53 +0100

    resolved conflicts
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

---
+++ public/pages/Rules/components/RuleContentViewer/RuleContentYamlViewer.test.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { RuleContentYamlViewer } from './RuleContentYamlViewer';
+
+describe('<RuleContentYamlViewer /> spec', () => {
+  it('renders the component', () => {
+    const { container } = render(
+      <RuleContentYamlViewer
+        rule={{
+          id: '25b9c01c-350d-4b95-bed1-836d04a4f324',
+          category: 'windows',
+          title: 'Testing rule',
+          description: 'Testing Description',
+          status: 'experimental',
+          author: 'Bhabesh Raj',
+          references: [
+            {
+              value: 'https://securelist.com/operation-tunnelsnake-and-moriya-rootkit/101831',
+            },
+          ],
+          tags: [
+            {
+              value: 'attack.persistence',
+            },
+            {
+              value: 'attack.privilege_escalation',
+            },
+            {
+              value: 'attack.t1543.003',
+            },
+          ],
+          log_source: '',
+          detection:
+            'selection:\n  Provider_Name: Service Control Manager\n  EventID: 7045\n  ServiceName: ZzNetSvc\ncondition: selection\n',
+          level: 'high',
+          false_positives: [
+            {
+              value: 'Unknown',
+            },
+          ],
+        }}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
