dae16fa49ef379de30c8a066b26f9fdb79ca408e
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-01-13 00:06:27 +0530

    consolidated most shared types at top level (#347)
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/services/OpenSearchService.ts
+++ public/services/OpenSearchService.ts
@@ -9,7 +9,6 @@ import {
   SimpleSavedObject,
 } from 'opensearch-dashboards/public';
 import { ServerResponse } from '../../server/models/types';
-import { SearchResponse, Plugin } from '../../server/models/interfaces';
 import { API } from '../../server/utils/constants';
 
 export default class OpenSearchService {
@@ -17,28 +16,6 @@ export default class OpenSearchService {
     private httpClient: HttpSetup,
     private savedObjectsClient: SavedObjectsClientContract
   ) {}
-
-  documentIdsQuery = async (
-    index: string,
-    documentIds: string[]
-  ): Promise<ServerResponse<SearchResponse<any>>> => {
-    if (!index || !documentIds) return;
-    return (await this.httpClient.get(`..${API.DOCUMENT_IDS_QUERY}`, {
-      query: { index, documentIds },
-    })) as ServerResponse<SearchResponse<any>>;
-  };
-
-  timeRangeQuery = async (
-    index: string,
-    timeField = 'timestamp',
-    startTime = 'now-15m',
-    endTime = 'now'
-  ): Promise<ServerResponse<SearchResponse<any>>> => {
-    if (!index) return;
-    return (await this.httpClient.get(`..${API.TIME_RANGE_QUERY}`, {
-      query: { index, timeField, startTime, endTime },
-    })) as ServerResponse<SearchResponse<any>>;
-  };
 
   getPlugins = async (): Promise<ServerResponse<Plugin[]>> => {
     let url = `..${API.PLUGINS}`;
