7788f848e287ce544edd861a88dd32d1d2ee02f8
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-01-12 11:22:26 +0100

    [TASK] Investigate cypress windows latest tests #351
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// /<reference types="cypress" />
+// eslint-disable-next-line
+///<reference types="cypress" />
 
 declare namespace Cypress {
   interface Chainable<Subject> {
