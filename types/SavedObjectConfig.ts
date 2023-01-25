04654d824d05bc9fd2a67a9b654d6b5fa51d14d1
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-01-24 17:50:50 -0800

    added types for configs
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

---
+++ types/SavedObjectConfig.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  SavedObjectReference,
+  SavedObjectsMigrationVersion,
+  SavedObjectAttributes,
+} from '../../../src/core/public';
+
+interface SavedObjectCreationConfigBase {
+  references: SavedObjectReference[];
+  migrationVersion?: SavedObjectsMigrationVersion;
+  id?: string;
+}
+
+export interface IndexPatternSavedObjectCreationConfig extends SavedObjectCreationConfigBase {
+  type: 'index-pattern';
+  attributes: {
+    fields: string;
+    timeFieldName: string;
+    title: string;
+  };
+}
+
+export interface VisualizationSavedObjectCreationConfig extends SavedObjectCreationConfigBase {
+  type: 'visualization';
+  attributes: SavedObjectAttributes & {
+    title: string;
+    description: string;
+    uiStateJSON: string;
+    visState: string;
+    kibanaSavedObjectMeta: {
+      searchSourceJSON: string;
+    };
+  };
+}
+
+export interface DashboardSavedObjectCreationConfig extends SavedObjectCreationConfigBase {
+  type: 'dashboard';
+  attributes: SavedObjectAttributes & {
+    title: string;
+    description: string;
+    hits: number;
+    optionsJSON: string;
+    panelsJSON: string;
+    visState: string;
+    kibanaSavedObjectMeta: {
+      searchSourceJSON: string;
+    };
+  };
+}
+
+export type SavedObjectCreationConfig =
+  | IndexPatternSavedObjectCreationConfig
+  | VisualizationSavedObjectCreationConfig
+  | DashboardSavedObjectCreationConfig;
