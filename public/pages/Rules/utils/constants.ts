e3fe84678b47e46b974efd505bbd0959a3890eaf
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-02-20 20:07:10 -0800

    removed commented code
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/pages/Rules/utils/constants.ts
+++ public/pages/Rules/utils/constants.ts
@@ -3,16 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const ruleTypes: { label: string; value: string }[] = [
-  // { label: 'Network', value: 'network' },
-  // { label: 'DNS', value: 'dns' },
-  // { label: 'Apache Access', value: 'apache_access' },
-  // { label: 'Windows', value: 'windows' },
-  // { label: 'AD/LDAP', value: 'ad_ldap' },
-  // { label: 'Linux', value: 'linux' },
-  // { label: 'Cloudtrail', value: 'cloudtrail' },
-  // { label: 'S3', value: 's3' },
-];
+export const ruleTypes: { label: string; value: string }[] = [];
 
 export const ruleSeverity: { name: string; value: string }[] = [
   { name: 'Critical', value: 'critical' },
