9816140275c6bdaf31addbe3309f4861ad9ee1c3
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-02-21 14:10:05 +0100

    unit tests fix
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- public/pages/Detectors/components/UpdateAlertConditions/UpdateAlertConditions.test.tsx
+++ public/pages/Detectors/components/UpdateAlertConditions/UpdateAlertConditions.test.tsx
@@ -4,14 +4,28 @@
  */
 
 import React from 'react';
-import { render } from '@testing-library/react';
 import props from '../../../../../test/mocks/Detectors/components/UpdateAlertConditions/UpdateAlertConditions.mock';
 import { expect } from '@jest/globals';
 import UpdateAlertConditions from './UpdateAlertConditions';
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+import { contextServicesMock } from '../../../../../test/mocks/useContext.mock';
+
+jest.mock(
+  '../../../CreateDetector/components/ConfigureAlerts/containers/ConfigureAlerts.tsx',
+  () => () => {
+    return <mock-component mock="ConfigureAlerts" />;
+  }
+);
 
 describe('<UpdateAlertConditions /> spec', () => {
-  it('renders the component', () => {
-    const view = render(<UpdateAlertConditions {...props} />);
-    expect(view).toMatchSnapshot();
+  it('renders the component', async () => {
+    let wrapper;
+    await act(async () => {
+      UpdateAlertConditions.contextType = React.createContext(contextServicesMock);
+      wrapper = await mount(<UpdateAlertConditions {...props} />);
+    });
+    wrapper.update();
+    expect(wrapper).toMatchSnapshot();
   });
 });
