a561fdacdd7958d97b84ae679ac6937ccd643121
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-02-21 22:24:10 +0100

    unit tests fix
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- test/mocks/services/ruleService.mock.ts
+++ test/mocks/services/ruleService.mock.ts
@@ -1,5 +1,6 @@
 import httpClientMock from './httpClient.mock';
 import { RuleService } from '../../../public/services';
+import _ from 'lodash';
 
 const ruleServiceMock = {
   getRules: () =>
@@ -9,6 +10,46 @@ const ruleServiceMock = {
         hits: {
           hits: [],
         },
+      },
+    }),
+  getAllRuleCategories: () =>
+    Promise.resolve({
+      ok: true,
+      response: {
+        rule_categories: [
+          {
+            display_name: 'Network',
+            key: 'network',
+          },
+          {
+            display_name: 'DNS',
+            key: 'dns',
+          },
+          {
+            display_name: 'Apache Access',
+            key: 'apache_access',
+          },
+          {
+            display_name: 'Windows',
+            key: 'windows',
+          },
+          {
+            display_name: 'AD/LDAP',
+            key: 'ad_ldap',
+          },
+          {
+            display_name: 'Linux',
+            key: 'linux',
+          },
+          {
+            display_name: 'Cloudtrail',
+            key: 'cloudtrail',
+          },
+          {
+            display_name: 'S3',
+            key: 's3',
+          },
+        ],
       },
     }),
 };
