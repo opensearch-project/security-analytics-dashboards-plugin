0d7362e5bf3153ebc2a2e5f0c77cab3ee8c05f70
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-03-26 22:34:47 +0200

    [FEATURE] Create global state object for async requests #491
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- cypress/integration/1_detectors.spec.js
+++ cypress/integration/1_detectors.spec.js
@@ -18,6 +18,145 @@ const testMappings = {
 
 const cypressDNSRule = dns_rule_data.title;
 
+const createDetector = (detectorName, dataSource, expectFailure) => {
+  // Locate Create detector button click to start
+  cy.get('.euiButton').filter(':contains("Create detector")').click({ force: true });
+
+  // Check to ensure process started
+  cy.waitForPageLoad('create-detector', {
+    contains: 'Define detector',
+  });
+
+  // Enter a name for the detector in the appropriate input
+  cy.get(`input[placeholder="Enter a name for the detector."]`).focus().realType(detectorName);
+
+  // Select our pre-seeded data source (check cypressIndexDns)
+  cy.get(`[data-test-subj="define-detector-select-data-source"]`)
+    .find('input')
+    .focus()
+    .realType(dataSource);
+
+  cy.intercept({
+    pathname: '/_plugins/_security_analytics/rules/_search',
+    query: {
+      prePackaged: 'true',
+    },
+  }).as('getSigmaRules');
+
+  // Select threat detector type (Windows logs)
+  cy.get(`input[id="dns"]`).click({ force: true });
+
+  cy.wait('@getSigmaRules').then(() => {
+    // Open Detection rules accordion
+    cy.get('[data-test-subj="detection-rules-btn"]').click({ force: true, timeout: 5000 });
+
+    cy.contains('table tr', 'DNS', {
+      timeout: 120000,
+    });
+
+    // find search, type USB
+    cy.get(`input[placeholder="Search..."]`).ospSearch(cypressDNSRule);
+
+    // Disable all rules
+    cy.contains('tr', cypressDNSRule, { timeout: 1000 });
+    cy.get('table th').within(() => {
+      cy.get('button').first().click({ force: true });
+    });
+
+    // Enable single rule
+    cy.contains('table tr', cypressDNSRule).within(() => {
+      cy.get('button').eq(1).click({ force: true, timeout: 2000 });
+    });
+  });
+
+  // Click Next button to continue
+  cy.get('button').contains('Next').click({ force: true });
+
+  // Check that correct page now showing
+  cy.contains('Configure field mapping');
+
+  if (!expectFailure) {
+    // Select appropriate names to map fields to
+    for (let field_name in testMappings.properties) {
+      const mappedTo = testMappings.properties[field_name].path;
+
+      cy.contains('tr', field_name).within(() => {
+        cy.get(`[data-test-subj="detector-field-mappings-select"]`).click().type(mappedTo);
+      });
+    }
+  }
+
+  // Continue to next page
+  cy.get('button').contains('Next').click({ force: true, timeout: 2000 });
+
+  // Check that correct page now showing
+  cy.contains('Set up alerts');
+
+  // Type name of new trigger
+  cy.get(`input[placeholder="Enter a name for the alert condition."]`)
+    .focus()
+    .realType('test_trigger');
+
+  // Type in (or select) tags for the alert condition
+  cy.get(`[data-test-subj="alert-tags-combo-box"]`)
+    .find('input')
+    .focus()
+    .realType('attack.defense_evasion')
+    .realPress('Enter');
+
+  // Select applicable severity levels
+  cy.get(`[data-test-subj="security-levels-combo-box"]`).click({ force: true });
+  cy.contains('1 (Highest)').click({ force: true });
+
+  // Continue to next page
+  cy.contains('Next').click({ force: true });
+
+  // Confirm page is reached
+  cy.contains('Review and create');
+
+  // Confirm field mappings registered
+  cy.contains('Field mapping');
+
+  if (!expectFailure) {
+    for (let field in testMappings.properties) {
+      const mappedTo = testMappings.properties[field].path;
+
+      cy.contains(field);
+      cy.contains(mappedTo);
+    }
+  }
+
+  // Confirm entries user has made
+  cy.contains('Detector details');
+  cy.contains(detectorName);
+  cy.contains('dns');
+  cy.contains(dataSource);
+  cy.contains('Alert on test_trigger');
+
+  // Create the detector
+  cy.get('button').contains('Create').click({ force: true });
+  cy.waitForPageLoad('detector-details', {
+    contains: detectorName,
+  });
+
+  cy.contains('Attempting to create the detector.');
+
+  // Confirm detector active
+  cy.contains(detectorName);
+  cy.contains('Active');
+
+  if (!expectFailure) {
+    cy.contains('Actions');
+  }
+
+  cy.contains('Detector configuration');
+  cy.contains('Field mappings');
+  cy.contains('Alert triggers');
+  cy.contains('Detector details');
+  cy.contains('Created at');
+  cy.contains('Last updated time');
+};
+
 describe('Detectors', () => {
   const cypressIndexDns = 'cypress-index-dns';
   const cypressIndexWindows = 'cypress-index-windows';
@@ -90,132 +229,13 @@ describe('Detectors', () => {
   });
 
   it('...can be created', () => {
-    // Locate Create detector button click to start
-    cy.get('.euiButton').filter(':contains("Create detector")').click({ force: true });
+    createDetector(detectorName, cypressIndexDns, false);
+    cy.contains('Detector created successfully');
+  });
 
-    // Check to ensure process started
-    cy.waitForPageLoad('create-detector', {
-      contains: 'Define detector',
-    });
-
-    // Enter a name for the detector in the appropriate input
-    cy.get(`input[placeholder="Enter a name for the detector."]`).focus().realType('test detector');
-
-    // Select our pre-seeded data source (check cypressIndexDns)
-    cy.get(`[data-test-subj="define-detector-select-data-source"]`)
-      .find('input')
-      .focus()
-      .realType(cypressIndexDns);
-
-    cy.intercept({
-      pathname: '/_plugins/_security_analytics/rules/_search',
-      query: {
-        prePackaged: 'true',
-      },
-    }).as('getSigmaRules');
-
-    // Select threat detector type (Windows logs)
-    cy.get(`input[id="dns"]`).click({ force: true });
-
-    cy.wait('@getSigmaRules').then(() => {
-      // Open Detection rules accordion
-      cy.get('[data-test-subj="detection-rules-btn"]').click({ force: true, timeout: 5000 });
-
-      cy.contains('table tr', 'DNS', {
-        timeout: 120000,
-      });
-
-      // find search, type USB
-      cy.get(`input[placeholder="Search..."]`).ospSearch(cypressDNSRule);
-
-      // Disable all rules
-      cy.contains('tr', cypressDNSRule, { timeout: 1000 });
-      cy.get('table th').within(() => {
-        cy.get('button').first().click({ force: true });
-      });
-
-      // Enable single rule
-      cy.contains('table tr', cypressDNSRule).within(() => {
-        cy.get('button').eq(1).click({ force: true, timeout: 2000 });
-      });
-    });
-
-    // Click Next button to continue
-    cy.get('button').contains('Next').click({ force: true });
-
-    // Check that correct page now showing
-    cy.contains('Configure field mapping');
-
-    // Select appropriate names to map fields to
-    for (let field_name in testMappings.properties) {
-      const mappedTo = testMappings.properties[field_name].path;
-
-      cy.contains('tr', field_name).within(() => {
-        cy.get(`[data-test-subj="detector-field-mappings-select"]`).click().type(mappedTo);
-      });
-    }
-
-    // Continue to next page
-    cy.get('button').contains('Next').click({ force: true, timeout: 2000 });
-
-    // Check that correct page now showing
-    cy.contains('Set up alerts');
-
-    // Type name of new trigger
-    cy.get(`input[placeholder="Enter a name for the alert condition."]`)
-      .focus()
-      .realType('test_trigger');
-
-    // Type in (or select) tags for the alert condition
-    cy.get(`[data-test-subj="alert-tags-combo-box"]`)
-      .find('input')
-      .focus()
-      .realType('attack.defense_evasion')
-      .realPress('Enter');
-
-    // Select applicable severity levels
-    cy.get(`[data-test-subj="security-levels-combo-box"]`).click({ force: true });
-    cy.contains('1 (Highest)').click({ force: true });
-
-    // Continue to next page
-    cy.contains('Next').click({ force: true });
-
-    // Confirm page is reached
-    cy.contains('Review and create');
-
-    // Confirm field mappings registered
-    cy.contains('Field mapping');
-
-    for (let field in testMappings.properties) {
-      const mappedTo = testMappings.properties[field].path;
-
-      cy.contains(field);
-      cy.contains(mappedTo);
-    }
-
-    // Confirm entries user has made
-    cy.contains('Detector details');
-    cy.contains(detectorName);
-    cy.contains('dns');
-    cy.contains(cypressIndexDns);
-    cy.contains('Alert on test_trigger');
-
-    // Create the detector
-    cy.get('button').contains('Create').click({ force: true });
-    cy.waitForPageLoad('detector-details', {
-      contains: detectorName,
-    });
-
-    // Confirm detector active
-    cy.contains(detectorName);
-    cy.contains('Active');
-    cy.contains('Actions');
-    cy.contains('Detector configuration');
-    cy.contains('Field mappings');
-    cy.contains('Alert triggers');
-    cy.contains('Detector details');
-    cy.contains('Created at');
-    cy.contains('Last updated time');
+  it('...can fail creation', () => {
+    createDetector(`${detectorName}_fail`, '.kibana_1', true);
+    cy.contains('Create detector failed.');
   });
 
   it('...basic details can be edited', () => {
