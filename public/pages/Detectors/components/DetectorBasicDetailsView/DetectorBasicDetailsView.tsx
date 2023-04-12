520074dad181c21b07b74cea9ba9b8994ac98bc7
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-04-12 16:31:23 +0200

    [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- public/pages/Detectors/components/DetectorBasicDetailsView/DetectorBasicDetailsView.tsx
+++ public/pages/Detectors/components/DetectorBasicDetailsView/DetectorBasicDetailsView.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 import { ContentPanel } from '../../../../components/ContentPanel';
 import { createTextDetailsGroup, parseSchedule } from '../../../../utils/helpers';
 import moment from 'moment';
-import { DEFAULT_EMPTY_DATA } from '../../../../utils/constants';
+import { DEFAULT_EMPTY_DATA, logTypesWithDashboards } from '../../../../utils/constants';
 import { Detector } from '../../../../../types';
 
 export interface DetectorBasicDetailsViewProps {
@@ -48,6 +48,8 @@ export const DetectorBasicDetailsView: React.FC<DetectorBasicDetailsViewProps> =
           {`${name} summary`}
           <EuiIcon type={'popout'} />
         </EuiLink>
+      ) : !logTypesWithDashboards.has(detector_type) ? (
+        'Not available for this log type'
       ) : (
         '-'
       )) as any,
