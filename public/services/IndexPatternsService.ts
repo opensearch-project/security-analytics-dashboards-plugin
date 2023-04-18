d0b170280ab8d12d1c596b9fe620e8f53d851955
AWSHurneyt <hurneyt@amazon.com>
2023-04-18 14:11:26 -0700

    [Backport 2.x] 2.7 manual backports. (#532)
    * Minor bug fixes (#505)
    
    * [BUG] Edit details Save button should be blue #499
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [BUG] Detector details has some bad text handling #496
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [BUG] Detector details has some bad text handling #496
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [BUG] Detector details has some bad text handling #496
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * [BUG] Fixes few minor UX bugs (#525)
    
    * [BUG] No space between the detector details and the rule panel #522
    [BUG] A rule flyout without references have an empty link #521
    [FEATURE] Update header size to use euiTitle--small #520
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [BUG] No space between the detector details and the rule panel #522
    [BUG] A rule flyout without references have an empty link #521
    [FEATURE] Update header size to use euiTitle--small #520
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Feature] deleting detectors should delete all related dashboards (including index patterns and visualisations) (#515)
    
    * Update detector details component #504
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [FEATURE] Deleting detectors should delete all related dashboards (including index-patterns and visualisations) #509
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Resolved merge conflicts, generated updated snapshot files, resolved discrepancies between main and 2.x branch.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Resolved merge conflicts, generated updated snapshot files, resolved discrepancies between main and 2.x branch.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- public/services/IndexPatternsService.ts
+++ public/services/IndexPatternsService.ts
@@ -8,6 +8,8 @@ import {
   IndexPatternSpec,
   IndexPatternsService as CoreIndexPatternsService,
 } from '../../../../src/plugins/data/common/index_patterns';
+import { IndexPatternSavedObjectAttrs } from '../../../../src/plugins/data/common/index_patterns/index_patterns';
+import { SavedObject } from '../../../../src/plugins/data/common';
 
 export default class IndexPatternsService {
   constructor(private coreIndexPatternsService: CoreIndexPatternsService) {}
@@ -19,4 +21,33 @@ export default class IndexPatternsService {
   async createAndSave(spec: IndexPatternSpec, override = false, skipFetchFields = false) {
     return this.coreIndexPatternsService.createAndSave(spec, override, skipFetchFields);
   }
+
+  public getIndexPatterns = async (): Promise<
+    SavedObject<IndexPatternSavedObjectAttrs>[] | null | undefined
+  > => {
+    const indexPatterns = await this.coreIndexPatternsService.getCache();
+
+    return Promise.resolve(indexPatterns);
+  };
+
+  public getIndexPattern = async (
+    detectorId: string
+  ): Promise<Promise<SavedObject<IndexPatternSavedObjectAttrs>> | Promise<undefined>> => {
+    let indexPattern;
+    const indexPatterns = await this.getIndexPatterns();
+    console.log('indexPatterns', indexPatterns);
+    indexPatterns?.some((indexRef) => {
+      if (indexRef.references.findIndex((reference) => reference.id === detectorId) > -1) {
+        indexPattern = indexRef;
+        return true;
+      }
+
+      return false;
+    });
+
+    return indexPattern;
+  };
+
+  public deleteIndexPattern = async (indexPatternId: string) =>
+    await this.coreIndexPatternsService.delete(indexPatternId);
 }
