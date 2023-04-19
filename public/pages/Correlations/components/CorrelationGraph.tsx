1dde91b7587d2d819641d27ffd899efe36f1675a
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-04-19 04:30:36 -0700

    removed redundant code
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/pages/Correlations/components/CorrelationGraph.tsx
+++ public/pages/Correlations/components/CorrelationGraph.tsx
@@ -3,39 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Graph from 'react-graph-vis';
-import 'vis-network/dist/dist/vis-network.min.css';
-import { loadFontAwesome } from '../utils/loadFonts';
 
 export const CorrelationGraph = ({ graph: { nodes, edges }, options, events }) => {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-  const [filteredNodes, setFilteredNodes] = useState(nodes);
-  const [graphVersion, setGraphVersion] = useState(0);
-
-  useEffect(() => {
-    setFilteredNodes(nodes);
-  }, [nodes]);
-  useEffect(() => {
-    setGraphVersion(graphVersion + 1);
-  }, [filteredNodes]);
-
-  useEffect(() => {
-    loadFontAwesome().then(() => {
-      setFontsLoaded(true);
-    });
-  }, []);
-
-  return fontsLoaded ? (
+  return (
     <Graph
-      key={`network-${graphVersion}`}
-      identifier={`network-${graphVersion}`}
-      graph={{ nodes: filteredNodes, edges }}
+      key={`network`}
+      identifier={`network`}
+      graph={{ nodes, edges }}
       options={options}
       events={events}
       style={{ border: '1px solid' }}
     />
-  ) : (
-    <p>Loading...</p>
   );
 };
--- public/pages/Correlations/utils/constants.tsx
+++ public/pages/Correlations/utils/constants.tsx
@@ -5,7 +5,6 @@
 
 import React from 'react';
 import { CorrelationGraphData } from '../../../../types';
-import { DETECTOR_TYPES } from '../../Detectors/utils/constants';
 import { ruleSeverity, ruleTypes } from '../../Rules/utils/constants';
 import { FilterItem } from '../components/FilterGroup';
 import { EuiIcon } from '@elastic/eui';
@@ -47,16 +46,6 @@ export const graphRenderOptions = {
   },
 };
 
-export enum TabIds {
-  CORRELATIONS = 'correlations',
-  CORRELATION_RULES = 'correlation-rules',
-}
-
-export const tabs = [
-  { id: TabIds.CORRELATIONS, name: 'Correlations' },
-  { id: TabIds.CORRELATION_RULES, name: 'Correlation rules' },
-];
-
 export const defaultLogTypeFilterItemOptions: FilterItem[] = Object.values(ruleTypes).map(
   (type) => {
     return {
@@ -80,29 +69,6 @@ export const defaultSeverityFilterItemOptions: FilterItem[] = Object.values(rule
     };
   }
 );
-
-export const iconByLogType = {
-  [DETECTOR_TYPES.AD_LDAP.id]: '\uf2b9',
-  [DETECTOR_TYPES.APACHE_ACCESS.id]: '\uf18c',
-  [DETECTOR_TYPES.AZURE.id]: '\uf0c2',
-  [DETECTOR_TYPES.CLOUD_TRAIL.id]: '\uf1be',
-  [DETECTOR_TYPES.DNS.id]: '\uf0ac',
-  [DETECTOR_TYPES.GITHUB.id]: '\uf09b',
-  [DETECTOR_TYPES.GWORKSPACE.id]: '\uf1a0',
-  [DETECTOR_TYPES.M365.id]: '\uf0b1',
-  [DETECTOR_TYPES.NETWORK.id]: '\uf0c0',
-  [DETECTOR_TYPES.OKTA.id]: '\uf2c2',
-  [DETECTOR_TYPES.S3.id]: '\uf0a0',
-  [DETECTOR_TYPES.SYSTEM.id]: '\uf1b3',
-  [DETECTOR_TYPES.WINDOWS.id]: '\uf109',
-};
-
-export const sizeBySeverity = {
-  Critical: 65,
-  Medium: 50,
-  Info: 35,
-  Low: 20,
-};
 
 export const emptyGraphData: CorrelationGraphData = {
   graph: {
