0d41bd71ef028c6ca2136fc9b6b45a37124e4e66
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-05-31 20:18:25 +0200

    bugfix for tags validation
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- public/pages/Rules/components/RuleEditor/components/YamlRuleEditorComponent/RuleTagsComboBox.tsx
+++ public/pages/Rules/components/RuleEditor/components/YamlRuleEditorComponent/RuleTagsComboBox.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { EuiFormRow, EuiText, EuiComboBox, EuiComboBoxOptionOption } from '@elastic/eui';
 
 export interface RuleTagsComboBoxProps {
@@ -16,12 +16,25 @@ export interface RuleTagsComboBoxProps {
   selectedOptions: EuiComboBoxOptionOption<string>[];
 }
 
+const STARTS_WITH = 'attack.';
+
+const isValid = (value: string) => {
+  if (value === '') return true;
+  return value.startsWith(STARTS_WITH) && value.length > STARTS_WITH.length;
+};
+
 export const RuleTagsComboBox: React.FC<RuleTagsComboBoxProps> = ({
   onCreateOption,
   onBlur,
   onChange,
   selectedOptions,
 }) => {
+  const [isCurrentlyTypingValueInvalid, setIsCurrentlyTypingValueInvalid] = useState(false);
+
+  const onSearchChange = (searchValue: string) => {
+    setIsCurrentlyTypingValueInvalid(!isValid(searchValue));
+  };
+
   return (
     <>
       <EuiFormRow
@@ -31,15 +44,22 @@ export const RuleTagsComboBox: React.FC<RuleTagsComboBoxProps> = ({
             <i>- optional</i>
           </EuiText>
         }
+        isInvalid={isCurrentlyTypingValueInvalid}
+        error={isCurrentlyTypingValueInvalid ? 'Invalid tag' : ''}
+        helpText={`Tags must start with '${STARTS_WITH}'`}
       >
         <EuiComboBox
           noSuggestions
+          onSearchChange={onSearchChange}
           placeholder="Create tags"
           onChange={onChange}
-          onCreateOption={(searchValue, options) => onCreateOption(searchValue, options)}
+          onCreateOption={(searchValue, options) =>
+            isValid(searchValue) && onCreateOption(searchValue, options)
+          }
           onBlur={onBlur}
           data-test-subj={'rule_tags_dropdown'}
           selectedOptions={selectedOptions}
+          isInvalid={isCurrentlyTypingValueInvalid}
         />
       </EuiFormRow>
     </>
