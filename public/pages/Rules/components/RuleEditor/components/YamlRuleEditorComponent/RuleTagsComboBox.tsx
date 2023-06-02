d9ca756263a6412b0efcc78e95e72f822401104a
AWSHurneyt <hurneyt@amazon.com>
2023-06-02 16:01:52 -0700

    Fixed missing import. (#621)
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- public/pages/Rules/components/RuleEditor/components/YamlRuleEditorComponent/RuleTagsComboBox.tsx
+++ public/pages/Rules/components/RuleEditor/components/YamlRuleEditorComponent/RuleTagsComboBox.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { EuiFormRow, EuiText, EuiComboBox, EuiComboBoxOptionOption } from '@elastic/eui';
 
 export interface RuleTagsComboBoxProps {
