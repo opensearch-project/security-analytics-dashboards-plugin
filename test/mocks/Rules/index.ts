f56ff33b680d324884774fb7275cb625b8ae0f20
Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
2023-06-07 19:24:16 +0200

    Cypress cases for detectors and rules, validate forms and fields
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- test/mocks/Rules/index.ts
+++ test/mocks/Rules/index.ts
@@ -6,9 +6,11 @@
 import RuleOptions from './RuleOptions.mock';
 import RulePage from './RulePage.mock';
 import SelectionExpField from './components/RuleEditor/components/SelectionExpField.mock';
+import DetectionVisualEditor from './components/RuleEditor/DetectionVisualEditor.mock';
 
 export default {
   RuleOptions,
   RulePage,
   SelectionExpField,
+  DetectionVisualEditor,
 };
