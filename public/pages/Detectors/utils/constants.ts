e7c1b91afa9e0d5db2028a976b84f9cd025c1692
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-07-31 16:36:52 -0700

    [Custom log types] Support custom log types in detection rule creation and detector creation (#676)
    * log types table; details page with basic editing
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * implemented create, delete of log type
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * detection rules; delete modals added
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated snapshots
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * addressed PR comments
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated cypress workflow
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * use log types from API
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated detector cypress test
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * using api provided log types in rule creation
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * addressed PR comments
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated cypress test
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated nav panel
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * fixed log type labels; cypress tests
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    (cherry picked from commit 53885963461ec2f0828062bb5b9d9ee627c5dcc1)

--- public/pages/Detectors/utils/constants.ts
+++ public/pages/Detectors/utils/constants.ts
@@ -25,20 +25,3 @@ export const EMPTY_DEFAULT_DETECTOR_INPUT = {
     rules: [],
   },
 };
-
-export const DETECTOR_TYPES = {
-  NETWORK: { id: 'network', label: 'Network events', abbr: 'NTW' },
-  DNS: { id: 'dns', label: 'DNS logs', abbr: 'DNS' },
-  APACHE_ACCESS: { id: 'apache_access', label: 'Apache access logs', abbr: 'APC' },
-  WINDOWS: { id: 'windows', label: 'Windows logs', abbr: 'WIN' },
-  AD_LDAP: { id: 'ad_ldap', label: 'AD/LDAP logs', abbr: 'AD' },
-  SYSTEM: { id: 'linux', label: 'System logs', abbr: 'LNX' },
-  CLOUD_TRAIL: { id: 'cloudtrail', label: 'Cloud Trail logs', abbr: 'CLT' },
-  S3: { id: 's3', label: 'S3 access logs', abbr: 'S3' },
-  GWORKSPACE: { id: 'gworkspace', label: 'Google Workspace logs', abbr: 'GGL' },
-  GITHUB: { id: 'github', label: 'Github actions', abbr: 'GHB' },
-  M365: { id: 'm365', label: 'Microsoft 365 logs', abbr: 'MSO' },
-  OKTA: { id: 'okta', label: 'Okta events', abbr: 'OKT' },
-  AZURE: { id: 'azure', label: 'Azure logs', abbr: 'AZR' },
-  VPC_FLOW: { id: 'vpcflow', label: 'VPC Flow logs', abbr: 'VPC' },
-};
