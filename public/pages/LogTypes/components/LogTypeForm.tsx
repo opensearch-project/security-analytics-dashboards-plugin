dd1e88f9281de1e076581434909105a21a0e558d
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-07-31 11:20:44 -0700

    [Custom log types] CRUD operations for log types (#675)
    * log types table; details page with basic editing
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * implemented create, delete of log type
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * detection rules; delete modals added
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated snapshots
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * addressed PR comments
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated cypress workflow
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated detector cypress test
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * addressed PR comments
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

---
+++ public/pages/LogTypes/components/LogTypeForm.tsx
@@ -0,0 +1,124 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  EuiBottomBar,
+  EuiButton,
+  EuiButtonEmpty,
+  EuiFieldText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFormRow,
+  EuiSpacer,
+  EuiTextArea,
+} from '@elastic/eui';
+import { LogTypeItem } from '../../../../types';
+import React from 'react';
+import { validateName } from '../../../utils/validation';
+import { NotificationsStart } from 'opensearch-dashboards/public';
+import { useState } from 'react';
+
+export interface LogTypeFormProps {
+  logTypeDetails: LogTypeItem;
+  isEditMode: boolean;
+  confirmButtonText: string;
+  notifications: NotificationsStart;
+  setLogTypeDetails: (logType: LogTypeItem) => void;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+export const LogTypeForm: React.FC<LogTypeFormProps> = ({
+  logTypeDetails,
+  isEditMode,
+  confirmButtonText,
+  notifications,
+  setLogTypeDetails,
+  onCancel,
+  onConfirm,
+}) => {
+  const [nameError, setNameError] = useState('');
+
+  const updateErrors = (details = logTypeDetails) => {
+    const nameInvalid = !validateName(details.name);
+    setNameError(nameInvalid ? 'Invalid name' : '');
+
+    return { nameInvalid };
+  };
+  const onConfirmClicked = () => {
+    const { nameInvalid } = updateErrors();
+
+    if (nameInvalid) {
+      notifications?.toasts.addDanger({
+        title: `Failed to ${confirmButtonText.toLowerCase()}`,
+        text: `Fix the marked errors.`,
+        toastLifeTimeMs: 3000,
+      });
+
+      return;
+    }
+    onConfirm();
+  };
+
+  return (
+    <>
+      <EuiFormRow
+        label="Name"
+        helpText={
+          isEditMode &&
+          'Must contain 5-50 characters. Valid characters are a-z, A-Zm 0-9, hyphens, spaces, and underscores'
+        }
+        isInvalid={!!nameError}
+        error={nameError}
+      >
+        <EuiFieldText
+          value={logTypeDetails?.name}
+          onChange={(e) => {
+            const newLogType = {
+              ...logTypeDetails!,
+              name: e.target.value,
+            };
+            setLogTypeDetails(newLogType);
+            updateErrors(newLogType);
+          }}
+          placeholder="Enter name for the log type"
+          disabled={!isEditMode || !!logTypeDetails.detectionRules}
+        />
+      </EuiFormRow>
+      <EuiSpacer />
+      <EuiFormRow label="Description">
+        <EuiTextArea
+          value={logTypeDetails?.description}
+          onChange={(e) => {
+            const newLogType = {
+              ...logTypeDetails!,
+              description: e.target.value,
+            };
+            setLogTypeDetails(newLogType);
+            updateErrors(newLogType);
+          }}
+          placeholder="Description of the log type"
+          disabled={!isEditMode}
+        />
+      </EuiFormRow>
+      {isEditMode ? (
+        <EuiBottomBar>
+          <EuiFlexGroup gutterSize="s" justifyContent="flexEnd">
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty color="ghost" size="s" iconType="cross" onClick={onCancel}>
+                Cancel
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiButton color="primary" fill iconType="check" size="s" onClick={onConfirmClicked}>
+                {confirmButtonText}
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiBottomBar>
+      ) : null}
+    </>
+  );
+};
