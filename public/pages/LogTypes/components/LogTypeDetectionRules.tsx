b54bb36229da42ae7f37a41d2aec78e75a0b5a45
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-08-01 20:39:13 -0700

    UI polish for correlations and custom log types (#683)
    * fixed abbreviations
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * fixed log type name validation
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/pages/LogTypes/components/LogTypeDetectionRules.tsx
+++ public/pages/LogTypes/components/LogTypeDetectionRules.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 import { RulesTable } from '../../Rules/components/RulesTable/RulesTable';
 import { RuleTableItem } from '../../Rules/utils/helpers';
 import { ContentPanel } from '../../../components/ContentPanel';
-import { EuiButton } from '@elastic/eui';
+import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiIcon, EuiSpacer, EuiText } from '@elastic/eui';
 
 export interface LogTypeDetectionRulesProps {
   rules: RuleTableItem[];
@@ -26,7 +26,28 @@ export const LogTypeDetectionRules: React.FC<LogTypeDetectionRulesProps> = ({
       hideHeaderBorder={true}
       actions={[<EuiButton onClick={refreshRules}>Refresh</EuiButton>]}
     >
-      <RulesTable loading={loadingRules} ruleItems={rules} showRuleDetails={() => {}} />
+      {rules.length === 0 ? (
+        <EuiFlexGroup justifyContent="center" alignItems="center" direction="column">
+          <EuiFlexItem grow={false}>
+            <EuiText color="subdued">
+              <p>There are no detection rules associated with this log type. </p>
+            </EuiText>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              fill
+              href={`opensearch_security_analytics_dashboards#/create-rule`}
+              target="_blank"
+            >
+              Create detection rule&nbsp;
+              <EuiIcon type={'popout'} />
+            </EuiButton>
+            <EuiSpacer size="xl" />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      ) : (
+        <RulesTable loading={loadingRules} ruleItems={rules} showRuleDetails={() => {}} />
+      )}
     </ContentPanel>
   );
 };
