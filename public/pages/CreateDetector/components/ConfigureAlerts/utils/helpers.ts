47e1af724b118c4abdad9a6311a702d37faf9b9a
Amardeepsingh Siglani <amardeep7194@gmail.com>
2024-03-13 00:10:05 -0700

    fetching channel types using API; updated type imports
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/pages/CreateDetector/components/ConfigureAlerts/utils/helpers.ts
+++ public/pages/CreateDetector/components/ConfigureAlerts/utils/helpers.ts
@@ -5,10 +5,10 @@
 
 import { EuiComboBoxOptionOption } from '@elastic/eui';
 import { ALERT_SEVERITY_OPTIONS, CHANNEL_TYPES } from './constants';
-import { FeatureChannelList } from '../../../../../../server/models/interfaces/Notifications';
 import { NotificationChannelTypeOptions } from '../models/interfaces';
 import { NotificationsService } from '../../../../../services';
 import { AlertCondition, TriggerAction } from '../../../../../../models/interfaces';
+import { FeatureChannelList } from '../../../../../../types';
 
 export const parseAlertSeverityToOption = (severity: string): EuiComboBoxOptionOption<string> => {
   return Object.values(ALERT_SEVERITY_OPTIONS).find(
