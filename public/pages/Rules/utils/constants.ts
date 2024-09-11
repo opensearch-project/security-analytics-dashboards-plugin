6f7cd2c8d98778b229159d7d46dd011e7003f2fc
Joanne Wang <jowg@amazon.com>
2024-09-11 15:52:39 -0700

    Fix findings page crash and rule severity correctness (#1160)
    * bug fix
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * fix correlation finding severity
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    Co-authored-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/pages/Rules/utils/constants.ts
+++ public/pages/Rules/utils/constants.ts
@@ -15,6 +15,22 @@ export const ruleTypes: {
 
 const paletteColors = euiPaletteForStatus(5);
 
+export enum RuleSeverityValue {
+  Critical = 'critical',
+  High = 'high',
+  Medium = 'medium',
+  Low = 'low',
+  Informational = 'informational',
+}
+
+export const RuleSeverityPriority: Record<RuleSeverityValue, string> = {
+  [RuleSeverityValue.Critical]: '1',
+  [RuleSeverityValue.High]: '2',
+  [RuleSeverityValue.Medium]: '3',
+  [RuleSeverityValue.Low]: '4',
+  [RuleSeverityValue.Informational]: '5',
+};
+
 export const ruleSeverity: {
   name: string;
   value: string;
@@ -23,32 +39,32 @@ export const ruleSeverity: {
 }[] = [
   {
     name: 'Critical',
-    value: 'critical',
-    priority: '1',
+    value: RuleSeverityValue.Critical,
+    priority: RuleSeverityPriority[RuleSeverityValue.Critical],
     color: { background: paletteColors[4], text: 'white' },
   },
   {
     name: 'High',
-    value: 'high',
-    priority: '2',
+    value: RuleSeverityValue.High,
+    priority: RuleSeverityPriority[RuleSeverityValue.High],
     color: { background: paletteColors[3], text: 'white' },
   },
   {
     name: 'Medium',
-    value: 'medium',
-    priority: '3',
+    value: RuleSeverityValue.Medium,
+    priority: RuleSeverityPriority[RuleSeverityValue.Medium],
     color: { background: paletteColors[2], text: 'black' },
   },
   {
     name: 'Low',
-    value: 'low',
-    priority: '4',
+    value: RuleSeverityValue.Low,
+    priority: RuleSeverityPriority[RuleSeverityValue.Low],
     color: { background: paletteColors[1], text: 'white' },
   },
   {
     name: 'Informational',
-    value: 'informational',
-    priority: '5',
+    value: RuleSeverityValue.Informational,
+    priority: RuleSeverityPriority[RuleSeverityValue.Informational],
     color: { background: paletteColors[0], text: 'white' },
   },
 ];
