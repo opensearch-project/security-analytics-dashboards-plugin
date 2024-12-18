251e69591e87642250e77e7c903c4ef657586b4a
Amardeepsingh Siglani <amardeep7194@gmail.com>
2024-12-18 15:40:49 -0800

    Remove threat intel checkbox detector creation (#1232)
    * removed threat intel from detector creation
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * fixed trigger edit; show callout in detector details view
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * work in progress
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated logic
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * removed unused prop
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * simplified changes
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated snapshot
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/pages/CreateDetector/components/DefineDetector/components/ThreatIntelligence/ThreatIntelligence.tsx
+++ public/pages/CreateDetector/components/DefineDetector/components/ThreatIntelligence/ThreatIntelligence.tsx
@@ -3,10 +3,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
-import { EuiCompressedCheckbox, EuiText, EuiTitle, htmlIdGenerator } from '@elastic/eui';
+import React, { useMemo, useState } from 'react';
+import {
+  EuiCallOut,
+  EuiCompressedCheckbox,
+  EuiLink,
+  EuiSpacer,
+  EuiText,
+  htmlIdGenerator,
+} from '@elastic/eui';
+import { buildRouteUrl } from '../../../../../../utils/helpers';
+import { ROUTES, THREAT_INTEL_NAV_ID } from '../../../../../../utils/constants';
 
 export interface ThreatIntelligenceProps {
+  isEdit: boolean;
   threatIntelChecked: boolean;
   onThreatIntelChange: (checked: boolean) => void;
 }
@@ -14,25 +24,67 @@ export interface ThreatIntelligenceProps {
 export const ThreatIntelligence: React.FC<ThreatIntelligenceProps> = ({
   threatIntelChecked,
   onThreatIntelChange,
+  isEdit,
 }) => {
+  const [shouldShowEditUI] = useState(isEdit && threatIntelChecked);
+  const threatIntelUrl = useMemo(() => {
+    return buildRouteUrl(THREAT_INTEL_NAV_ID, ROUTES.THREAT_INTEL_OVERVIEW);
+  }, []);
+
   return (
     <>
-      <EuiTitle size="m">
-        <h3>Threat intelligence feeds</h3>
-      </EuiTitle>
+      {!shouldShowEditUI && (
+        <>
+          <EuiText size="m">
+            <h3>Threat intelligence feeds</h3>
+          </EuiText>
+          <EuiText size="s">
+            <p>
+              To match your data source against known indicators of compromise configure logs scan
+              with threat intel sources on the{' '}
+              <EuiLink target="_blank" href={threatIntelUrl}>
+                Threat intelligence
+              </EuiLink>{' '}
+              page.
+            </p>
+          </EuiText>
+        </>
+      )}
+      {shouldShowEditUI && (
+        <>
+          <EuiText size="m">
+            <h3>Threat intelligence feeds</h3>
+          </EuiText>
 
-      <EuiText size="s">
-        <p>
-          Match your data source against known malicious IP-addresses. Available for standard log
-          types only.
-        </p>
-      </EuiText>
-      <EuiCompressedCheckbox
-        id={htmlIdGenerator()()}
-        label="Enable threat intelligence-based detection"
-        checked={threatIntelChecked}
-        onChange={(e) => onThreatIntelChange(e.target.checked)}
-      />
+          <EuiText size="s">
+            <p>
+              Match your data source against known malicious IP-addresses. Available for standard
+              log types only.
+            </p>
+          </EuiText>
+          <EuiSpacer size="s" />
+          <EuiCallOut
+            size="s"
+            title={
+              <p>
+                To match your data against known indicators of compromise we recommend configuring
+                scan using the new{' '}
+                <EuiLink target="_blank" href={threatIntelUrl}>
+                  Threat Intelligence
+                </EuiLink>{' '}
+                platform and disabling threat intelligence in the detector.
+              </p>
+            }
+          />
+          <EuiSpacer size="s" />
+          <EuiCompressedCheckbox
+            id={htmlIdGenerator()()}
+            label="Enable threat intelligence-based detection"
+            checked={threatIntelChecked}
+            onChange={(e) => onThreatIntelChange(e.target.checked)}
+          />
+        </>
+      )}
     </>
   );
 };
