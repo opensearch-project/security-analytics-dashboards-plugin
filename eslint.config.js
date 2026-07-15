3fbf5bda44588d71235d3d98ced9ebaf41aaac77
SuZhou-Joe <suzhou@amazon.com>
2026-07-15 08:21:47 +0800

    Adopt ESLint 10 / flat config (#1552)
    * Adopt ESLint 10 / flat config
    
    Migrate to flat eslint.config.js spreading @elastic/eslint-config-kibana. Delete the stale, deprecated @elastic/kibana YAML config. Closes #1551
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * Remove eslint-plugin-no-unsanitized pin (inherited from OSD)
    
    OSD core provides eslint-plugin-no-unsanitized (^4.1.5) via the shared config. The plugin's own ^3.0.2 pin conflicts under yarn --single-version, breaking bootstrap. Remove it so the version is inherited from OSD, consistent with eslint/prettier/cypress.
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * Update yarn.lock after removing no-unsanitized pin
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    ---------
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const osdConfig = require('@elastic/eslint-config-kibana');
+
+module.exports = [...osdConfig];
